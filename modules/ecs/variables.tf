bbef247fa208a20f8bd3ec100d6c87186a8e231c
Oky Sabeni <oky.sabeni@gmail.com>
2018-08-31 14:16:50 -0700

    completed the ecs variables section

---
+++ modules/ecs/variables.tf
@@ -0,0 +1,47 @@
+variable "environment" {
+  description = "The environment"
+}
+
+variable "vpc_id" {
+  description = "The VPC id"
+}
+
+variable "availability_zones" {
+  type = "list"
+  description = "the azs to use"
+}
+
+variable "security_group_ids" {
+  type = "list"
+  description = "The SGs to use"
+}
+
+variable "subnet_ids" {
+  type = "list"
+  description = "the private subnets to use"
+}
+
+variable "public_subnet_ids" {
+  type = "list"
+  description = "the private subnets to use"
+}
+
+variable "database_endpoint" {
+  description = "the database endpoint"
+}
+
+variable "database_username" {
+  description = "the database username"
+}
+
+variable "database_password" {
+  description = "the database password"
+}
+
+variable "repository_name" {
+  description = "the name of the repository"
+}
+
+variable "secret_key_base" {
+  description = "the secret key base to use in the app"
+}
