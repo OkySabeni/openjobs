b2daf069a246609c2c67facada8d5437c969024a
Oky Sabeni <oky.sabeni@gmail.com>
2018-09-02 08:44:23 -0700

    completed main pipeline tf

---
+++ pipeline.tf
@@ -0,0 +1,13 @@
+module "code_pipeline" {
+  source = "./module/code_pipeline"
+  repository_url = "${module.ecs.repository_url}"
+  region = "${var.region}"
+  ecs_service_name = "${module.ecs.service_name}"
+  ecs_cluster_name = "${module.ecs.cluster_name}"
+  run_task_subnet_id = "${module.networking.private_subnet_ids[0]}"
+  run_task_security_group_ids = [
+    "${module.networking.security_group_ids}",
+    "${module.rds.db_access_sg_id}",
+    "${module.ecs.security_group_id}"
+  ]
+}
